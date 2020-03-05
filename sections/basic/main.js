9e0e6e4455d8788f7e562e13cc5ace445b140622
Leonardo <lmarzochi@soujmv.com>
2020-03-04 22:21:45 -0300

    feat(basic): add script

---
+++ sections/basic/main.js
@@ -0,0 +1,26 @@
+const listRepos = async username => {
+  const repos = await fetch(
+    `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
+  )
+    .then(res => res.json())
+    .catch(err => console.log(err));
+
+  const markup = repos
+    .map(
+      repo => `
+            <li>
+                <a href="${repo.html_url}">${repo.name}</a>
+                (${repo.stargazers_count})
+            </li>    
+        `
+    )
+    .join("");
+
+  const content = document.getElementById("content");
+  console.log(content);
+  
+
+  content.innerHTML = `<ul>${markup}</ul>`;
+};
+
+listRepos('leomarzochi');
